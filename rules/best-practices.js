ca54b2283414b32dd1e36f0cf7c79966d8cb8d2e
joefitter <hello@joefitter.com>
2016-06-21 11:54:29 +0100

    [bugfix] - duplicate no-labels rule

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -23,7 +23,6 @@ module.exports = {
     'no-floating-decimal': 2,
     'no-implied-eval': 2,
     'no-iterator': 2,
-    'no-labels': 0,
     'no-lone-blocks': 2,
     'no-loop-func': 2,
     'no-multi-spaces': 2,
